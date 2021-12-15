b25ab33b0f3177f62587b5cc4865e07cd8d3dbf1
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-15 13:53:42 +0900

    week2

---
+++ CodeStarterCamp_Week2/main.swift
@@ -0,0 +1,42 @@
+//
+//  main.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+
+import Foundation
+
+// 빈  Int Array 생성
+/*
+print("로또 번호:", terminator: " ")
+ var lottoInt: Array<Int> = []
+ for _ in 1 ...45
+while lottoNumbers.count < 6 {
+ var lottoNumbers:[Int] = []
+    var lottoNum = Int.random(in: 1...45) 
+}
+
+
+ */
+/*
+print("게임 수: ", terminator:" ")
+var input = readLine()
+var input_num = Int(input!)!
+for _ in 1...input_num {
+    var numbers:[Int] = []
+    while numbers.count < 6 { // 사용
+        var number = Int.random(in: 1...45)
+        if !numbers.contains(number) {
+            numbers.append(number)
+            
+        }
+        print(numbers.sorted())
+        
+    }
+
+    }
+ */
+
+
